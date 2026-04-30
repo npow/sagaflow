ff1e9bbcc36f8bde42c7b2697698a4545e11bc23
Nissan Pow <nissan.pow@gmail.com>
2026-04-30 05:49:08 +0000

    fix: use _log instead of undefined logger variable

--- sagaflow/worker.py
+++ sagaflow/worker.py
@@ -470,6 +470,6 @@ async def run_worker(*, target: str = DEFAULT_TARGET) -> None:
     from sagaflow.manifest import cleanup_stale_runs
     cleaned = cleanup_stale_runs()
     if cleaned:
-        logger.info('cleaned up %d stale runs on startup', cleaned)
+        _log.info('cleaned up %d stale runs on startup', cleaned)
 
     await worker.run()
